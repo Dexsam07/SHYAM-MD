8e8520069ca7ff2148f43a69474c067ab9ca44f8
Dexsam07 <txgaminglover2@gmail.com>
2026-01-29 19:34:39 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,22 @@
+const settings = {
+  prefixes: ['.'], // Multiple prefix support you can add one or more
+  packname: 'SHYAM-MD',
+  author: '‎dexsam07',
+  timeZone: 'Asia/kalkata',
+  botName: "SHYAM-MD", // your bot name
+  botOwner: 'Dex Shyam Chaudhari', // set your name
+  ownerNumber: '923051391007', // Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20,
+  tempCleanupInterval: 1 * 60 * 60 * 1000, // 1 hours
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "5.1.0",
+  updateZipUrl: "https://github.com/Dexsam07/SHYAM-MD/archive/refs/heads/main.zip",
+  channelLink: "https://whatsapp.com/channel/0029VbBgXTsKwqSKZKy38w2o",
+  ytch: "Dexsam07"
+};
+
+module.exports = settings;
+
